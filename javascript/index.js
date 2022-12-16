f703e8412e7fd90bc4889eca70e4adf82ca2d9df
Shaheen Badiyan <shaheen.badiyan@gmail.com>
2022-12-16 13:08:32 -0600

    added target parameter

--- javascript/index.js
+++ javascript/index.js
@@ -1,33 +1,29 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1Kn8IWWkYaGBOdH4aspl4AbHWNMqQQVp0wnQ_nDmqFxg/edit#gid=0';
 
-function loadData() {
-    // Load all hitters and format with DataTables.
-    $("#data-table").sheetrock({
-        url: mySpreadsheet,
-        query: "select B,C,F,N,S order by C desc",
-    }).on('sheetrock:loaded', function () {
-        $(this).DataTable({
-            columns: [
-            {
-                "title":"Image",
-                "render": function (data, type, row, meta) {
-                return '<img src="' + data + '" width="200" height="200">';
-                }
-            },
-            {"title":"Item Number"},
-            {"title":"Asking Price"},
-            {"title":"Item Name"},
-            {
-                "title":"Additional Info", 
-                "render": function (data, type, row, meta) {
-                return '<a href="' + data + '">Link</a>';
-                }
-            },
-            
-            ],
-            "pageLength": 25,
-        });
+$("#data-table").sheetrock({
+    url: mySpreadsheet,
+    query: "select B,C,F,N,S order by C desc",
+    target: document.getElementById("data-table")
+}).on('sheetrock:loaded', function () {
+    $(this).DataTable({
+        columns: [
+        {
+            "title":"Image",
+            "render": function (data, type, row, meta) {
+            return '<img src="' + data + '" width="200" height="200">';
+            }
+        },
+        {"title":"Item Number"},
+        {"title":"Asking Price"},
+        {"title":"Item Name"},
+        {
+            "title":"Additional Info", 
+            "render": function (data, type, row, meta) {
+            return '<a href="' + data + '">Link</a>';
+            }
+        },
+        
+        ],
+        "pageLength": 25,
     });
-}
-
-loadData();
+});
