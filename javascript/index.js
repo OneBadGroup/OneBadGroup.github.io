d2b1121be428d4579617da83033796b541bce98d
Shaheen Badiyan <shaheen.badiyan@gmail.com>
2022-12-16 12:12:47 -0600

    bug fix?

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,33 @@
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1Kn8IWWkYaGBOdH4aspl4AbHWNMqQQVp0wnQ_nDmqFxg/edit#gid=0';
 
-// Load all hitters and format with DataTables.
-$('#data-table').sheetrock({
-    url: mySpreadsheet,
-    query: "select B,C,F,N,S order by C desc",
-  }).on('sheetrock:loaded', function () {
-    $(this).DataTable({
-        columns: [
-          {
-            "title":"Image",
-            "render": function (data, type, row, meta) {
-              return '<img src="' + data + '" width="200" height="200">';
-            }
-          },
-          {"title":"Item Number"},
-          {"title":"Asking Price"},
-          {"title":"Item Name"},
-          {
-            "title":"Additional Info", 
-            "render": function (data, type, row, meta) {
-              return '<a href="' + data + '">Link</a>';
-            }
-          },
-          
-        ],
-        "pageLength": 25,
+function loadData() {
+    // Load all hitters and format with DataTables.
+    $("#data-table").sheetrock({
+        url: mySpreadsheet,
+        query: "select B,C,F,N,S order by C desc",
+    }).on('sheetrock:loaded', function () {
+        $(this).DataTable({
+            columns: [
+            {
+                "title":"Image",
+                "render": function (data, type, row, meta) {
+                return '<img src="' + data + '" width="200" height="200">';
+                }
+            },
+            {"title":"Item Number"},
+            {"title":"Asking Price"},
+            {"title":"Item Name"},
+            {
+                "title":"Additional Info", 
+                "render": function (data, type, row, meta) {
+                return '<a href="' + data + '">Link</a>';
+                }
+            },
+            
+            ],
+            "pageLength": 25,
+        });
     });
-  });
+}
+
+loadData();
