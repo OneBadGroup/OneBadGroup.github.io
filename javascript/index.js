0d13d120c22004f20a46109b250586c9de0e262a
Shaheen Badiyan <shaheen.badiyan@gmail.com>
2022-12-20 13:59:35 -0600

    removed duplicates; more styling improvements

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,4 @@
-var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1Kn8IWWkYaGBOdH4aspl4AbHWNMqQQVp0wnQ_nDmqFxg/edit#gid=0';
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1Kn8IWWkYaGBOdH4aspl4AbHWNMqQQVp0wnQ_nDmqFxg/edit#gid=738902544';
 
 var table = $("#data-table").sheetrock({
     url: mySpreadsheet,
@@ -12,22 +12,41 @@ table.on('sheetrock:loaded', function () {
         {
             "title":"Image",
             "render": function (data, type, row, meta) {
-            return '<img src="' + data + '" width="200" height="200">';
+                return '<img src="' + data + '" width="200" height="200">';
             }
         },
-        {"title":"Item Number"},
-        {"title":"Quantity Available"},
-        {"title":"Asking Price"},
-        {"title":"Item Name"},
+        {
+            "title":"Item Number",
+            "render": function (data, type, row, meta) {
+                return "Item # " + data;
+            }
+        },
+        {
+            "title":"Quantity Available",
+            "render": function (data, type, row, meta) {
+                if(data < 2)
+                    return '<p style="color:red;"><b>' + data + " Remaining</b></p>";
+                else
+                    return '<p style="color:black;">' + data + " Remaining</p>";
+            }
+        },
+        {
+            "title":"Asking Price",
+            "render": function (data, type, row, meta) {
+                return '<b>' + data + '</b>'
+            }
+        },
+        {"title":"Item Description"},
         {
             "title":"Additional Info", 
             "render": function (data, type, row, meta) {
-            return '<a href="' + data + '">Link</a>';
+                return '<a href="' + data + '">More Info</a>';
             }
         },
         
         ],
-        "pageLength": 10,
+        fixedHeader: false,
+        pageLength: 10,
         responsive: true
     });
 });
